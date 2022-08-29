7b047c24b6e061630d3f1880669ed426f80e8d1f
Aboubacar DJENIKA <75429330+geekdjenika@users.noreply.github.com>
2022-08-29 15:10:26 +0000

    Perfectionnement de l'insertion automatique des états du problème

--- src/main/java/com/ErrorNotes/Notes/Controllers/EtatController.java
+++ src/main/java/com/ErrorNotes/Notes/Controllers/EtatController.java
@@ -1,26 +1,10 @@
 package com.ErrorNotes.Notes.Controllers;
 
-import com.ErrorNotes.Notes.Services.EtatService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/etat")
 public class EtatController {
-
-    @Autowired
-    final private EtatService etatService;
-
-    @PostMapping("/insererEtat")
-    String InsererEtat(){
-        etatService.insertDefault();
-        return "Etat Inserer avec success";
-
-    }
-
 
 }
--- src/main/java/com/ErrorNotes/Notes/NotesApplication.java
+++ src/main/java/com/ErrorNotes/Notes/NotesApplication.java
@@ -3,6 +3,8 @@ package com.ErrorNotes.Notes;
 import com.ErrorNotes.Notes.Repositories.EtatRepository;
 import com.ErrorNotes.Notes.Services.EtatService;
 import com.ErrorNotes.Notes.Services.EtatServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.Modifying;
@@ -11,11 +13,19 @@ import org.springframework.data.jpa.repository.Query;
 import javax.transaction.Transactional;
 
 @SpringBootApplication
-public class NotesApplication {
+public class NotesApplication implements CommandLineRunner {
 
+	@Autowired
+	EtatRepository etatRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		if (etatRepository.findAll().size() == 0)
+		etatRepository.insererEtat();
+		else return;
+	}
 }
